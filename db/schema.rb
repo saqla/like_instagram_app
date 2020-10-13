d50f68d102c2cdd89e0e9905d5a97fd940b7c7cb
saqla <whitecross8fortune8fatedress@gmail.com>
2020-10-13 18:47:44 +0900

    users_table に　account カラムを追加 - 一意性を検証

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_10_12_085034) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "account", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
