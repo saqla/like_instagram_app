a49025c3a42b15b733fa1104b337ca0aa2cddaa6
saqla <whitecross8fortune8fatedress@gmail.com>
2020-10-21 18:14:39 +0900

    header & footerの追加 jqueryの準備

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,8 +9,54 @@ require("@rails/activestorage").start()
 require("channels")
 require('jquery')
 
+import $ from 'jquery'
+
 import 'src/js/sample.js'
 
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('aaaaaa')
+  $('.test-title').on('click', () => {
+    window.alert('CLICKED')
+  })
+})
+
+// jqueryボタンクリック
+// var btn1 = document.getElementById('btnJquery')
+// btn1.addEventListener('click', () => {
+//   console.log('jquery')
+  // chooseFile().then(f => {
+  //   const body = new FormData()
+  //   body.append('img', f)
+  //   body.append('foo', 'fuga')
+  //   $.ajax({
+  //     url: 'file.php',
+  //     method: 'post',
+  //     data: body,
+  //     success: data => {
+  //       result.innerHTML = data
+  //     },
+  //     contentType: false,
+  //     processData: false,
+  //   })
+  // })
+// })
+
+// -- 幻時刻表示 --
+// document.addEventListener('DOMContentLoaded', () => {
+//   $('.test-title').on('click', () => {
+//     var date = new Date();
+//     const utc = date.toUTCString();
+//     const g = utc.replace('GMT', '');
+//     const gDate = new Date(g);
+//     const hours = gDate.getHours();
+//     const time = gDate.setHours(hours + 9);
+//     const strTime = strTime.toString();
+
+//     const jTime = strTime.split(' '); ←split出来ない
+
+//     window.alert(jTime[4]);
+//   })
+// })
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
